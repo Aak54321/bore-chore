f2a2d58c1ef53198024dcfa270b212c0b5c62c20
Aakash Shalini <128037309+Aak54321@users.noreply.github.com>
2024-04-19 19:55:59 +0530

    ffmpeg integrate

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
-
+import ffmpeg from "fluent-ffmpeg";
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-app.get('/',(req,res)=> {
-    res.send("hello")
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request");
+  }
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      return res.status(200).send(`Process Complete`);
+    })
+    .on("error", () => {
+      res.status(500).send(`Internal Server Error`);
+    })
+    .save(outputFilePath);
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Server running at ${port}`);
-})
+});
